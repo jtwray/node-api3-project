3d4fa2bb8b199ca75d42484395a0d527f7517622
tucker <tuckerwray84@gmail.com>
2020-03-03 05:49:36 -0500

    remove id and reservation.reservatoin_pkey totally maybe it auto fills in

--- data/seeds/004-reservation.js
+++ data/seeds/004-reservation.js
@@ -4,178 +4,143 @@ exports.seed = function (knex) {
     [{
       rvowner_id: 14,
       listing_id: 16,
-      date: '12/6/2019',
-      reservation_pkey: '16,14,12/6/2019'
+      date: '12/6/2019'
     }, {
       rvowner_id: 20,
       listing_id: 15,
-      date: '9/8/2022',
-      reservation_pkey: '15,20,9/8/2022'
+      date: '9/8/2022'
     }, {
       rvowner_id: 4,
       listing_id: 19,
-      date: '11/11/2020',
-      reservation_pkey: '19,4,11/11/2020'
+      date: '11/11/2020'
     }, {
       rvowner_id: 4,
       listing_id: 7,
-      date: '12/28/2021',
-      reservation_pkey: '7,4,12/28/2021'
+      date: '12/28/2021'
     }, {
       rvowner_id: 4,
       listing_id: 18,
-      date: '6/25/2022',
-      reservation_pkey: '18,4,6/25/2022'
+      date: '6/25/2022'
     }, {
       rvowner_id: 12,
       listing_id: 19,
-      date: '2/25/2022',
-      reservation_pkey: '19,12,2/25/2022'
+      date: '2/25/2022'
     }, {
       rvowner_id: 11,
       listing_id: 10,
-      date: '6/18/2019',
-      reservation_pkey: '10,11,6/18/2019'
+      date: '6/18/2019'
     }, {
       rvowner_id: 9,
       listing_id: 20,
-      date: '10/10/2021',
-      reservation_pkey: '20,9,10/10/2021'
+      date: '10/10/2021'
     }, {
       rvowner_id: 10,
       listing_id: 8,
-      date: '7/18/2020',
-      reservation_pkey: '8,10,7/18/2020'
+      date: '7/18/2020'
     }, {
       rvowner_id: 4,
       listing_id: 1,
-      date: '12/9/2020',
-      reservation_pkey: '1,4,12/9/2020'
+      date: '12/9/2020'
     }, {
       rvowner_id: 10,
       listing_id: 13,
-      date: '9/29/2020',
-      reservation_pkey: '13,10,9/29/2020'
+      date: '9/29/2020'
     }, {
       rvowner_id: 5,
       listing_id: 7,
-      date: '3/5/2019',
-      reservation_pkey: '7,5,3/5/2019'
+      date: '3/5/2019'
     }, {
       rvowner_id: 20,
       listing_id: 16,
-      date: '8/1/2021',
-      reservation_pkey: '16,20,8/1/2021'
+      date: '8/1/2021'
     }, {
       rvowner_id: 20,
       listing_id: 10,
-      date: '7/31/2021',
-      reservation_pkey: '10,20,7/31/2021'
+      date: '7/31/2021'
     }, {
       rvowner_id: 5,
       listing_id: 9,
-      date: '9/5/2021',
-      reservation_pkey: '9,5,9/5/2021'
+      date: '9/5/2021'
     }, {
       rvowner_id: 8,
       listing_id: 10,
-      date: '10/16/2019',
-      reservation_pkey: '10,8,10/16/2019'
+      date: '10/16/2019'
     }, {
       rvowner_id: 15,
       listing_id: 7,
-      date: '7/20/2020',
-      reservation_pkey: '7,15,7/20/2020'
+      date: '7/20/2020'
     }, {
       rvowner_id: 13,
       listing_id: 7,
-      date: '11/19/2020',
-      reservation_pkey: '7,13,11/19/2020'
+      date: '11/19/2020'
     }, {
       rvowner_id: 2,
       listing_id: 4,
-      date: '5/2/2020',
-      reservation_pkey: '4,2,5/2/2020'
+      date: '5/2/2020'
     }, {
       rvowner_id: 3,
       listing_id: 14,
-      date: '11/30/2019',
-      reservation_pkey: '14,3,11/30/2019'
+      date: '11/30/2019'
     }, {
       rvowner_id: 16,
       listing_id: 9,
-      date: '3/14/2020',
-      reservation_pkey: '9,16,3/14/2020'
+      date: '3/14/2020'
     }, {
       rvowner_id: 15,
       listing_id: 5,
-      date: '12/16/2019',
-      reservation_pkey: '5,15,12/16/2019'
+      date: '12/16/2019'
     }, {
       rvowner_id: 14,
       listing_id: 6,
-      date: '9/17/2019',
-      reservation_pkey: '6,14,9/17/2019'
+      date: '9/17/2019'
     }, {
       rvowner_id: 13,
       listing_id: 17,
-      date: '8/10/2022',
-      reservation_pkey: '17,13,8/10/2022'
+      date: '8/10/2022'
     }, {
       rvowner_id: 11,
       listing_id: 9,
-      date: '5/5/2021',
-      reservation_pkey: '9,11,5/5/2021'
+      date: '5/5/2021'
     }, {
       rvowner_id: 8,
       listing_id: 15,
-      date: '10/20/2022',
-      reservation_pkey: '15,8,10/20/2022'
+      date: '10/20/2022'
     }, {
       rvowner_id: 5,
       listing_id: 16,
-      date: '10/3/2022',
-      reservation_pkey: '16,5,10/3/2022'
+      date: '10/3/2022'
     }, {
       rvowner_id: 9,
       listing_id: 10,
-      date: '11/18/2021',
-      reservation_pkey: '10,9,11/18/2021'
+      date: '11/18/2021'
     }, {
       rvowner_id: 5,
       listing_id: 13,
-      date: '5/28/2021',
-      reservation_pkey: '13,5,5/28/2021'
+      date: '5/28/2021'
     }, {
       rvowner_id: 1,
       listing_id: 7,
-      date: '12/27/2019',
-      reservation_pkey: '7,1,12/27/2019'
+      date: '12/27/2019'
     }, {
       rvowner_id: 8,
       listing_id: 1,
-      date: '11/22/2020',
-      reservation_pkey: '1,8,11/22/2020'
+      date: '11/22/2020'
     }, {
       rvowner_id: 14,
       listing_id: 16,
-      date: '8/1/2019',
-      reservation_pkey: '16,14,8/1/2019'
+      date: '8/1/2019'
     }, {
       rvowner_id: 8,
       listing_id: 15,
-      date: '8/7/2020',
-      reservation_pkey: '15,8,8/7/2020'
+      date: '8/7/2020'
     }, {
       rvowner_id: 17,
       listing_id: 5,
-      date: '3/15/2022',
-      reservation_pkey: '5,17,3/15/2022'
+      date: '3/15/2022'
     }, {
       rvowner_id: 17,
       listing_id: 20,
-      date: '6/29/2022',
-      reservation_pkey: '20,17,6/29/2022'
+      date: '6/29/2022'
     }]
   )
 }
