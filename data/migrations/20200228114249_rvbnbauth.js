6cec5d522b6d0ed61730b7b5fd4545266daf2340
tucker <tuckerwray84@gmail.com>
2020-03-03 03:02:59 -0500

    migrations and seeds

--- data/migrations/20200228114249_rvbnbauth.js
+++ data/migrations/20200228114249_rvbnbauth.js
@@ -1,36 +1,118 @@
 exports.up = function (knex) {
   return knex.schema
-    .createTable('rv', rv => {
-      rv
+    .createTable('rv', tbl => {
+      tbl
         .increments()
-      rv
+      tbl
         .string('username', 255)
         .notNullable()
         .unique()
-      rv
+      tbl
         .string('email', 255)
         .notNullable()
         .unique()
-      rv.string('password', 255).notNullable()
+      tbl.string('password', 255).notNullable()
     })
 
-    .createTable('landOwner', landOwner => {
-      landOwner
+    .createTable('landOwner', tbl => {
+      tbl
         .increments()
-      landOwner
+      tbl
         .string('username', 255)
         .notNullable()
         .unique()
-      landOwner
+      tbl
         .string('email', 255)
         .notNullable()
         .unique()
-      landOwner.string('password', 255).notNullable()
-      landOwner.string('contact', 255).notNullable()
+      tbl
+        .string('password', 255)
+        .notNullable()
+      tbl
+        .string('contact', 255)
+        .notNullable()
+    })
+    .createTable('listing', tbl => {
+      tbl
+        .increments()
+      tbl
+        .string('description', 255)
+        .notNullable()
+      tbl
+        .integer('price', 255)
+        .unsigned()
+        .notNullable()
+      tbl
+        .string('photo', 255)
+      tbl
+        .string('amenities', 255)
+      tbl
+        .integer('landowner_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('landowner')
+    })
+    .createTable('reservation', tbl => {
+      tbl
+        .integer('rvowner_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('rv')
+      tbl
+        .integer('listing_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('listing')
+      tbl
+        .string('date', 255)
+        .notNullable()
+      tbl
+        .primary(['rvowner_id', 'listing_id', 'date'])
+
+    })
+    .createTable('landowner_listing', tbl => {
+      tbl
+        .integer('landowner_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('landowner')
+      tbl
+        .integer('listing_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('listing')
+      tbl
+        .primary(['landowner_id', 'listing_id'])
+    })
+    .createTable('rvownerfav_listing', tbl => {
+      tbl
+        .integer('rvowner_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('rv')
+      tbl
+        .integer('listing_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('listing')
+      tbl
+        .primary(['rvowner_id', 'listing_id'])
     })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('landOwner').dropTableIfExists('rv')
-
+  return knex.schema
+    .dropTableIfExists('rvownerfav_listing')
+    .dropTableIfExists('landowner_listing')
+    .dropTableIfExists('reservation')
+    .dropTableIfExists('listing')
+    .dropTableIfExists('landowner')
+    .dropTableIfExists('rv');
 }
