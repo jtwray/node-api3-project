54fa1a71e423b6eeb40f69373a1711ecb0e54d59
tucker <tuckerwray84@gmail.com>
2020-03-03 14:40:40 -0500

    meta tag to allow linkedin thumbnail and description

--- api/server.js
+++ api/server.js
@@ -20,7 +20,24 @@ server.use('/auth/rv', rvAuth);
 
 server.get('/api', (req, res) => {
   res.send(`   
-  
+<meta property='og:title' content='NODEjs|Express API for -- rVenture:the 5th wheel Airbnb -- a company that connects land owners and 5th wheel / RV owners."/>
+<meta property='og:image' content='https://imgur.com/hpzN3f8"/>
+<meta property='og:description' content='NODEjs|Express API for -- rVenture:the 5th wheel Airbnb --RV camping Airbnb
+NAME
+RV camping Airbnb
+PITCH
+5th wheel Airbnb is a company that connects land owners and 5th wheel / RV owners.  RV parks are often cramped and in many areas are booked months in advance. Collectively, landowners hold vast swaths of unused land that could be earning them revenue.  By using 5th wheel Airbnb, RV Owners get  access to use these previously unknown/unavailable sites, and Landowners get to cash in on otherwise dormant or underutilized land.
+MVP
+1. Users can register/create an account as either an **RV Owner** or a **Landowner** (web, mobile)
+2. **Landowners** and **RV Owners** can login to the the app. (web, mobile)
+3. **Landowners** can create, update and delete listings of their available land. At a minimum, a listing must include the land's location, description, price per day, and a photo. (web, mobile)
+4. **RV Owners** can query/filter available listings by location (at a minimum) and reserve a spot for their RV for a desired date(s) (web, mobile)
+STRETCH
+1. Use MapKit (iOS) or MapView (Android) to render available sites on a map that **RV Owners** can click on to view the listings. (mobile)
+2. **Landowners** and **RV Owners** can send messages to each other through the app. (web, mobile)
+3. **Landowners** can specify *Available Dates* or *Blackout Dates* for each property listed. (web) 
+	* **RV Owners** are only shown listings that are available on their desired date(s). (mobile)"/>
+<meta property='og:url' content='https://rventure.herokuapp.com/api/ of the article" />
 <h1>rVenture</h1>
 
 <h2>https://rventure.herokuapp.com/  ✔ api status</h2>
--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/blog.db3'
+      filename: './data/development-rventure.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
