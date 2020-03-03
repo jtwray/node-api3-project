bcd8d168438206db0650d0b55033ed322976f9c9
tucker <tuckerwray84@gmail.com>
2020-03-03 05:21:15 -0500

    cleanup data listing.js seed add underscore to landower_id

--- data/seeds/003-listing.js
+++ data/seeds/003-listing.js
@@ -2,140 +2,140 @@
 exports.seed = function (knex) {
   return knex('listing').insert([{
     id: 1,
-    landownerid: 9,
+    landowner_id: 9,
     description: 'harness viral communities',
     price: '$793.84',
     photo: 'http://dummyimage.com/149x149.png/ff4444/ffffff',
     amenities: 'restrooms'
   }, {
     id: 2,
-    landownerid: 8,
+    landowner_id: 8,
     description: 'matrix end-to-end bandwidth',
     price: '$686.76',
     photo: 'http://dummyimage.com/248x212.png/ff4444/ffffff',
     amenities: 'showers'
   }, {
     id: 3,
-    landownerid: 6,
+    landowner_id: 6,
     description: 'synthesize B2B applications',
     price: '$933.11',
     photo: 'http://dummyimage.com/140x191.jpg/ff4444/ffffff',
     amenities: 'water'
   }, {
     id: 4,
-    landownerid: 13,
+    landowner_id: 13,
     description: 'utilize holistic solutions',
     price: '$81.88',
     photo: 'http://dummyimage.com/249x177.bmp/5fa2dd/ffffff',
     amenities: 'wifi'
   }, {
     id: 5,
-    landownerid: 12,
+    landowner_id: 12,
     description: 'orchestrate wireless mindshare',
     price: '$764.96',
     photo: 'http://dummyimage.com/123x113.bmp/cc0000/ffffff',
     amenities: 'wifi'
   }, {
     id: 6,
-    landownerid: 11,
+    landowner_id: 11,
     description: 'orchestrate bricks-and-clicks experiences',
     price: '$891.48',
     photo: 'http://dummyimage.com/145x167.bmp/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 7,
-    landownerid: 6,
+    landowner_id: 6,
     description: 'visualize cross-media ROI',
     price: '$524.98',
     photo: 'http://dummyimage.com/115x249.bmp/dddddd/000000',
     amenities: 'electric'
   }, {
     id: 8,
-    landownerid: 16,
+    landowner_id: 16,
     description: 'engineer front-end e-services',
     price: '$160.48',
     photo: 'http://dummyimage.com/138x241.bmp/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 9,
-    landownerid: 10,
+    landowner_id: 10,
     description: 'disintermediate revolutionary portals',
     price: '$801.99',
     photo: 'http://dummyimage.com/174x178.png/ff4444/ffffff',
     amenities: 'wifi'
   }, {
     id: 10,
-    landownerid: 7,
+    landowner_id: 7,
     description: 'recontextualize web-enabled metrics',
     price: '$355.14',
     photo: 'http://dummyimage.com/236x214.png/dddddd/000000',
     amenities: 'electric'
   }, {
     id: 11,
-    landownerid: 13,
+    landowner_id: 13,
     description: 'evolve dynamic methodologies',
     price: '$429.90',
     photo: 'http://dummyimage.com/194x220.jpg/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 12,
-    landownerid: 11,
+    landowner_id: 11,
     description: 'benchmark transparent channels',
     price: '$961.21',
     photo: 'http://dummyimage.com/128x108.jpg/cc0000/ffffff',
     amenities: 'restrooms'
   }, {
     id: 13,
-    landownerid: 9,
+    landowner_id: 9,
     description: 'deliver extensible markets',
     price: '$426.17',
     photo: 'http://dummyimage.com/145x157.jpg/ff4444/ffffff',
     amenities: 'showers'
   }, {
     id: 14,
-    landownerid: 1,
+    landowner_id: 1,
     description: 'incubate seamless niches',
     price: '$759.08',
     photo: 'http://dummyimage.com/231x169.jpg/cc0000/ffffff',
     amenities: 'water'
   }, {
     id: 15,
-    landownerid: 7,
+    landowner_id: 7,
     description: 'facilitate distributed experiences',
     price: '$764.08',
     photo: 'http://dummyimage.com/191x109.jpg/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 16,
-    landownerid: 9,
+    landowner_id: 9,
     description: 'e-enable B2C supply-chains',
     price: '$746.34',
     photo: 'http://dummyimage.com/181x138.jpg/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 17,
-    landownerid: 12,
+    landowner_id: 12,
     description: 'syndicate bleeding-edge interfaces',
     price: '$357.39',
     photo: 'http://dummyimage.com/250x189.jpg/ff4444/ffffff',
     amenities: 'restrooms'
   }, {
     id: 18,
-    landownerid: 20,
+    landowner_id: 20,
     description: 'innovate killer architectures',
     price: '$78.57',
     photo: 'http://dummyimage.com/196x187.bmp/5fa2dd/ffffff',
     amenities: 'electric'
   }, {
     id: 19,
-    landownerid: 13,
+    landowner_id: 13,
     description: 'optimize back-end synergies',
     price: '$277.05',
     photo: 'http://dummyimage.com/106x156.jpg/5fa2dd/ffffff',
     amenities: 'water'
   }, {
     id: 20,
-    landownerid: 19,
+    landowner_id: 19,
     description: 'brand frictionless interfaces',
     price: '$170.02',
     photo: 'http://dummyimage.com/190x168.png/dddddd/000000',
