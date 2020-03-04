40e3960d57754eab2e2e426bfa600c1612dda96c
tucker <tuckerwray84@gmail.com>
2020-03-04 02:42:17 -0500

    comment out the email verification until its necessary

--- api/middleware/uniqueuserMiddleware.js
+++ api/middleware/uniqueuserMiddleware.js
@@ -11,32 +11,34 @@ module.exports = (req, res, next) => {
             message: `Sorry. That username: ' ${username} ' has been taken. Please choose another.`
           })
         } else {
-          email !== undefined &&
-            findBy({ email: email })
-              .then(user => {
-                if (user.length) {
-                  res.status(400).json({
-                    message:
-                      `Sorry. That email: ' ${email} ' has been taken. Please choose another.`
-                  })
-                } else next()
-              })
-              .catch(err =>
-                res.status(500).json({
-                  message:
-                    `Sorry. Something went wrong when validating this is a unique email.----${console.error(err)}----`,
-                  error: err,
-                  error_mesage: err.message
-                })
-              )
+          //   email !== undefined &&
+          //     findBy({ email: email })
+          //       .then(user => {
+          //         if (user.length) {
+          //           res.status(400).json({
+          //             message:
+          //               `Sorry. That email: ' ${email} ' has been taken. Please choose another.`
+          //           })
+          // else 
+          next()
         }
       })
       .catch(err =>
         res.status(500).json({
           message:
-            'Sorry. Something went wrong when validating this is a unique username.',
+            `Sorry. Something went wrong when validating this is a unique email.----${console.error(err)}----`,
           error: err,
           error_mesage: err.message
         })
+      )
+}
+      })
+      .catch (err =>
+res.status(500).json({
+  message:
+    'Sorry. Something went wrong when validating this is a unique username.',
+  error: err,
+  error_mesage: err.message
+})
       )
 }
