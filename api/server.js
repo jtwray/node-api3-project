839bd6a249205cfdc8edb8038865a85b17be74f7
tucker <tuckerwray84@gmail.com>
2020-03-04 00:36:01 -0500

    remove dependency express-metatag and related code

--- api/server.js
+++ api/server.js
@@ -4,15 +4,6 @@ const server = express()
 
 configMiddleware(server)
 
-var OG = require('express-metatag')('og')
-server.use(OG([{
-  title: 'NODEjs|Express API for -- rVenture:the 5th wheel Airbnb -- a company that connects land owners and 5th wheel / RV owners.',
-  description: `NAME|RV camping Airbnb
-  PITCH|
-  5th wheel Airbnb is a company that connects land owners and 5th wheel / RV owners.  RV parks are often cramped and in many areas are booked months in advance. Collectively, landowners hold vast swaths of unused land that could be earning them revenue.  By using 5th wheel Airbnb, RV owners get  access to use these previously unknown/unavailable sites, and Landowners get to cash in on otherwise dormant or underutilized land`,
-  image: 'https://imgur.com/hpzN3f8'
-}]
-))
 const rvRouter = require('../api/rv/rv-router.js')
 const rvAuth = require('../api/auth/auth-router-rv.js')
 const landownerAuth = require('../api/auth/auth-router-lo.js')
