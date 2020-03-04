e052f7e9e5830f86a6ab73feefa7d4a5304ddb26
tucker <tuckerwray84@gmail.com>
2020-03-04 00:08:17 -0500

    landowner/landowner-model

--- api/auth/auth-router-lo.js
+++ api/auth/auth-router-lo.js
@@ -1,63 +1,62 @@
 
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets');
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
 
-const { add, findBy, findById } = require('../landowner/landowner-model.js');
+const { add, findBy } = require('../landowner/landowner-model')
 
 //  CREATE
 
 router.post('/register/', (req, res) => {
-  const landowner = req.body;
+  const landowner = req.body
   // console.log(req)
-  const hash = bcrypt.hashSync(landowner.password, 10);
-  landowner.password = hash;
+  const hash = bcrypt.hashSync(landowner.password, 10)
+  landowner.password = hash
 
   add(landowner)
     .then(saved => {
-      res.status(201).json(saved);
+      res.status(201).json(saved)
     })
     .catch(error => {
       console.log(error)
-      res.status(500).json(error);
-    });
-});
+      res.status(500).json(error)
+    })
+})
 
 router.post('/login/', (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
   findBy({ username })
     .first()
     .then(landowner => {
       if (landowner && bcrypt.compareSync(password, landowner.password)) {
-
-        const token = genToken(landowner);
+        const token = genToken(landowner)
         res.status(200).json({
           message: `Welcome landowner ${landowner.username}!`,
           token: token
-        });
+        })
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'Invalid Credentials' })
       }
     })
     .catch(error => {
-      res.status(500).json(error);
-    });
-});
+      res.status(500).json(error)
+    })
+})
 
-function genToken(landowner) {
+function genToken (landowner) {
   const payload = {
     landownerid: landowner.id,
     username: landowner.username,
     roles: 'landowner'
-  };
+  }
   const options = {
     expiresIn: '1d'
-  };
+  }
 
-  const token = jwt.sign(payload, secrets.jwtSecrets, options);
-  return token;
+  const token = jwt.sign(payload, secrets.jwtSecrets, options)
+  return token
 }
 
-module.exports = router;
+module.exports = router
--- api/landOwner/landOwner-model.js
+++ api/landOwner/landOwner-model.js
@@ -1,26 +1,23 @@
-const db = require('../../data/dbConfig.js');
+const db = require('../../data/dbConfig.js')
 
 module.exports = {
-    add, find, findBy, findById,
-};
-
-function find() {
-    return db('landowner').select('id', 'username', 'password');
+  add, find, findBy, findById
 }
 
-function findBy(filter) {
-    return db('landowner')  
-    .where(filter);
+function find () {
+  return db('landowner').select('id', 'username', 'password')
 }
 
-function findById(id) {
-    return db('landowner').where({ id }).first();
+function findBy (filter) {
+  return db('landowner')
+    .where(filter)
 }
 
-async function add(user) {
-    const [id] = await db('landowner').insert(user);
-    return findById(id);
+function findById (id) {
+  return db('landowner').where({ id }).first()
 }
 
-
-
+async function add (user) {
+  const [id] = await db('landowner').insert(user)
+  return findById(id)
+}
