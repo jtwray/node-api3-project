edbb8e6bfafd34c8aaeb5e35fdd513089f37ff16
tucker <tuckerwray84@gmail.com>
2020-03-04 02:25:56 -0500

    temp solution for no email as an unrequired param. write checkfor middleware or just require the email in db

--- api/middleware/uniqueuserMiddleware.js
+++ api/middleware/uniqueuserMiddleware.js
@@ -1,42 +1,42 @@
 const { findBy } = require('../../api/landOwner/landOwner-model.js')
 
-
 module.exports = (req, res, next) => {
   const { username, email } = req.body
-
-  findBy({ username: username })
-    .then(user => {
-      if (user.length) {
-        console.log('user', user)
-        res.status(400).json({
-          message: `Sorry. That username: ' ${username} ' has been taken. Please choose another.`
-        })
-      } else {
-        findBy({ email: email })
-          .then(user => {
-            if (user.length) {
-              res.status(400).json({
-                message:
-                  `Sorry. That email: ' ${email} ' has been taken. Please choose another.`
-              })
-            } else next()
+  username !== undefined &&
+    findBy({ username: username })
+      .then(user => {
+        if (user.length) {
+          console.log('user', user)
+          res.status(400).json({
+            message: `Sorry. That username: ' ${username} ' has been taken. Please choose another.`
           })
-          .catch(err =>
-            res.status(500).json({
-              message:
-                `Sorry. Something went wrong when validating this is a unique email.----${console.error(err)}----`,
-              error: err,
-              error_mesage: err.message
-            })
-          )
-      }
-    })
-    .catch(err =>
-      res.status(500).json({
-        message:
-          'Sorry. Something went wrong when validating this is a unique username.',
-        error: err,
-        error_mesage: err.message
+        } else {
+          email !== undefined &&
+            findBy({ email: email })
+              .then(user => {
+                if (user.length) {
+                  res.status(400).json({
+                    message:
+                      `Sorry. That email: ' ${email} ' has been taken. Please choose another.`
+                  })
+                } else next()
+              })
+              .catch(err =>
+                res.status(500).json({
+                  message:
+                    `Sorry. Something went wrong when validating this is a unique email.----${console.error(err)}----`,
+                  error: err,
+                  error_mesage: err.message
+                })
+              )
+        }
       })
-    )
+      .catch(err =>
+        res.status(500).json({
+          message:
+            'Sorry. Something went wrong when validating this is a unique username.',
+          error: err,
+          error_mesage: err.message
+        })
+      )
 }
