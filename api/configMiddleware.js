415f70e7271d7ea0b22c508b7ed14195cca30fdc
tucker <tuckerwray84@gmail.com>
2020-03-04 00:18:07 -0500

    linting errors

--- api/configMiddleware.js
+++ api/configMiddleware.js
@@ -4,11 +4,9 @@ const helmet = require('helmet')
 const cors = require('cors')
 const logger = require('./logger-middleware')
 module.exports = function (server) {
-   
-server.use(helmet());
-server.use(express.json());
-server.use(morgan('dev'));
-server.use(cors());
-server.use(logger);
-
-};
+  server.use(helmet())
+  server.use(express.json())
+  server.use(morgan('dev'))
+  server.use(cors())
+  server.use(logger)
+}
