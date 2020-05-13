0e0452e638c4ef302fc6209b3762a3ec4372818b
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-05-13 00:36:42 -0400

    Update 20200228114249_rvbnbauth.js
    formats migration table for readability

--- data/migrations/20200228114249_rvbnbauth.js
+++ data/migrations/20200228114249_rvbnbauth.js
@@ -1,109 +1,101 @@
+
 exports.up = function (knex) {
   return knex.schema
-    .createTable('rv', tbl => {
-      tbl.increments()
-      tbl
-        .string('username', 255)
-        .notNullable()
-        .unique()
-      tbl.string('email', 255).unique()
-      tbl.string('contact', 255)
-      tbl.string('password', 255).notNullable()
+    .createTable("rv", (rv) => {
+      rv.increments();
+      rv.string("username", 255).notNullable().unique();
+      rv.string("email", 255).unique();
+      rv.string("contact", 255);
+      rv.string("password", 255).notNullable();
     })
-    .createTable('landowner', tbl => {
-      tbl.increments()
-      tbl
-        .string('username', 255)
-        .notNullable()
-        .unique()
-      tbl.string('email', 255).unique()
-      tbl.string('contact', 255)
-      tbl.string('password', 255).notNullable()
+    .createTable("landowner", (landowner) => {
+      landowner.increments();
+      landowner.string("username", 255).notNullable().unique();
+      landowner.string("email", 255).unique();
+      landowner.string("contact", 255);
+      landowner.string("password", 255).notNullable();
     })
-    .createTable('listing', tbl => {
-      tbl
-        .increments()
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl.text('description', 255).notNullable()
-      tbl.string('price', 255).notNullable()
-      tbl.string('location', 255).notNullable()
-      tbl.string('photo', 255)
-      tbl.string('amenities', 255)
-      tbl
-        .integer('landowner_id')
+    .createTable("listing", (listing) => {
+      listing.increments().onUpdate("CASCADE").onDelete("CASCADE");
+      listing.text("description", 255).notNullable();
+      listing.string("price", 255).notNullable();
+      listing.string("location", 255).notNullable();
+      listing.string("photo", 255);
+      listing.string("amenities", 255);
+      listing
+        .integer("landowner_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('landowner')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
+        .references("id")
+        .inTable("landowner")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     })
-    .createTable('reservation', tbl => {
-      tbl.increments()
-      tbl
-        .integer('rvowner_id')
+    .createTable("reservation", (reservation) => {
+      reservation.increments();
+      reservation
+        .integer("rvowner_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('rv')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl
-        .integer('listing_id')
+        .references("id")
+        .inTable("rv")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      reservation
+        .integer("listing_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('listing')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl.string('date', 255).notNullable()
+        .references("id")
+        .inTable("listing")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      reservation.string("date", 255).notNullable();
     })
-    .createTable('landowner_listing', tbl => {
-      tbl
-        .integer('landowner_id')
+    .createTable("landowner_listing", (landowner_listing) => {
+      landowner_listing
+        .integer("landowner_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('landowner')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl
-        .integer('listing_id')
+        .references("id")
+        .inTable("landowner")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      landowner_listing
+        .integer("listing_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('listing')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl.primary(['landowner_id', 'listing_id'])
+        .references("id")
+        .inTable("listing")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      landowner_listing.primary(["landowner_id", "listing_id"]);
     })
-    .createTable('rvownerfav_listing', tbl => {
-      tbl
-        .integer('rvowner_id')
+    .createTable("rvownerfav_listing", (rvownerfav_listing) => {
+      rvownerfav_listing
+        .integer("rvowner_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('rv')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl
-        .integer('listing_id')
+        .references("id")
+        .inTable("rv")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      rvownerfav_listing
+        .integer("listing_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('listing')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      tbl.primary(['rvowner_id', 'listing_id'])
-    })
+        .references("id")
+        .inTable("listing")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      rvownerfav_listing.primary(["rvowner_id", "listing_id"]);
+    });
 };
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('rvownerfav_listing')
-    .dropTableIfExists('landowner_listing')
-    .dropTableIfExists('reservation')
-    .dropTableIfExists('listing')
-    .dropTableIfExists('landowner')
-    .dropTableIfExists('rv')
+    .dropTableIfExists("rvownerfav_listing")
+    .dropTableIfExists("landowner_listing")
+    .dropTableIfExists("reservation")
+    .dropTableIfExists("listing")
+    .dropTableIfExists("landowner")
+    .dropTableIfExists("rv");
 };
